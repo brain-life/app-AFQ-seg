7ba3c1ba2144966e30edaf890ddaf2533c2af9ff
Aman Arya <aman.arya524@gmail.com>
2017-07-13 15:28:23 -0400

    Update main.py
    add bval, bvec changes and full streamlines

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-"""
-==========================
-Plotting tract profiles
-==========================
-An example of tracking and segmenting two tracts, and plotting their tract
-profiles for FA (calculated with DTI).
-"""
 import os.path as op
 import numpy as np
 import nibabel as nib
@@ -26,25 +19,28 @@ def main():
 	with open('config.json') as config_json:
 	    config = json.load(config_json)
 
+	#Paths to data
 	data_file = str(config['data_file'])
 	data_bval = str(config['data_bval'])
 	data_bvec = str(config['data_bvec'])
 
 	img = nib.load(data_file)
-
+	
+	"""
 	print("Calculating DTI...")
 	if not op.exists('./dti_FA.nii.gz'):
 	    dti_params = dti.fit_dti(data_file, data_bval, data_bvec, out_dir='.')
 	else:
 	    dti_params = {'FA': './dti_FA.nii.gz',
 			  'params': './dti_params.nii.gz'}
-
+	"""
 	#tg = nib.streamlines.load('track.trk').tractogram
-	tg = nib.streamlines.load(config['tck_data'])
-	streamlines = tg.tractogram.apply_affine(np.linalg.inv(img.affine)).streamlines
+	
+	tg = nib.streamlines.load(config['tck_data']).tractogram
+	streamlines = tg.apply_affine(np.linalg.inv(img.affine)).streamlines
 
 	# Use only a small portion of the streamlines, for expedience:
-	streamlines = streamlines[::100]
+	#streamlines = streamlines[::100]
 
 	templates = afd.read_templates()
 	bundle_names = ["CST", "ILF"]
@@ -58,12 +54,20 @@ def main():
 
 
 	print("Registering to template...")
+	if not op.exists('mapping.nii.gz'):
+        	gtab = dpg.gradient_table(hardi_fbval, hardi_fbvec)
+    	    	mapping = reg.syn_register_dwi(hardi_fdata, gtab)
+            	reg.write_mapping(mapping, './mapping.nii.gz')
+	else:
+    	    	mapping = reg.read_mapping('./mapping.nii.gz', img, MNI_T2_img)
+	
+	"""
 	MNI_T2_img = dpd.read_mni_template()
 	bvals, bvecs = read_bvals_bvecs(data_bval, data_bvec)
 	gtab = gradient_table(bvals, bvecs, b0_threshold=100)
 	mapping = reg.syn_register_dwi(data_file, gtab)
 	reg.write_mapping(mapping, './mapping.nii.gz')
-
+	"""
 
 	print("Segmenting fiber groups...")
 	fiber_groups = seg.segment(data_file,
@@ -74,8 +78,7 @@ def main():
 			           reg_template=MNI_T2_img,
 			           mapping=mapping,
 			           as_generator=False,
-				   affine = img.affine
-			           )
+				   affine = img.affine)
 	
 	path = os.getcwd() + '/tract1/'
         if not os.path.exists(path):
@@ -84,18 +87,8 @@ def main():
 	for fg in fiber_groups:
 	    	streamlines = fiber_groups[fg]
 		fname = fg + ".tck"
-		#aus.write_trk(fname, streamlines)
 		trg = nib.streamlines.Tractogram(streamlines, affine_to_rasmm=img.affine)
     		nib.streamlines.save(trg,path+fname)
-	"""
-	FA_img = nib.load(dti_params['FA'])
-	FA_data = FA_img.get_data()
-	print("Extracting tract profiles...")
-	for bundle in bundles:
-	    fig, ax = plt.subplots(1)
-	    profile = seg.calculate_tract_profile(FA_data, fiber_groups[bundle])
-	    ax.plot(profile)
-	    ax.set_title(bundle)
-	plt.savefig('bundle.png')
-	"""
+
+
 main()
